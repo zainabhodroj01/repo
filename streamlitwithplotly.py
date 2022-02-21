08f841d0a76ed48eac7b5e7adbb946702e8c8d62
zainabhodroj01 <99832089+zainabhodroj01@users.noreply.github.com>
2022-02-21 04:34:23 +0200

    Update streamlitwithplotly.py

--- streamlitwithplotly.py
+++ streamlitwithplotly.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 deaths = pd.read_csv("Death Cause Reason by Country.csv")
 import pandas as pd 
-import numpy as np 
 import plotly.express as px 
 import plotly as plt
 
